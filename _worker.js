42b20760fc80841d3ed9412f68707b02788d675e
Chenchen66-cmd <2417799582@qq.com>
2024-07-21 16:27:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,8 +12,7 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
-https://sub.xf.free.hr/auto
+vless://d33a6b68-1025-4c9f-9f6e-abb1530f9f95@185.133.35.231:443?encryption=none&security=tls&sni=vless1.chenchen.sbs&alpn=http%2F1.1&fp=random&type=ws&host=vless1.chenchen.sbs&path=%2F%3Fproxyip%3Dproxyip.oracle.fxxk.dedyn.io#BR%E3%80%90%E8%AF%B7%E5%8B%BF%E6%B5%8B%E9%80%9F%E3%80%91
 `
 
 let urls = [];
